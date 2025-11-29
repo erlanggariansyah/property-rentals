c8d163a0d0113877d5e559afa91d00812a4d7f72
erlanggariansyah <erlanggariansyah0@gmail.com>
2025-11-30 03:01:37 +0700

    Initial Commit

--- src/subComponents/Contact.jsx
+++ src/subComponents/Contact.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
-import { RiFacebookBoxLine } from "react-icons/ri";
 import { Link } from "react-router-dom";
+import { RiFacebookBoxLine } from "react-icons/ri";
 const SubContact = () => {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
